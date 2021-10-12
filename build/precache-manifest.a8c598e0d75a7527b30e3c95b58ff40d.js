23e60ac2cd41e2b9f94b77995d4a2c5cc038ea91
poomanii <aipoomani@gmail.com>
2021-10-12 13:26:55 +0530

    changes

---
+++ build/precache-manifest.a8c598e0d75a7527b30e3c95b58ff40d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe7224f3d7b61807858e83869481e9f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e229110bb87e14d40f8",
+    "url": "/static/css/main.4621ad3a.chunk.css"
+  },
+  {
+    "revision": "c98c50cf1a274e4a69d2",
+    "url": "/static/js/2.bc46d10e.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.bc46d10e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e229110bb87e14d40f8",
+    "url": "/static/js/main.433aa7d3.chunk.js"
+  },
+  {
+    "revision": "6fc4f2c3fd5ae8d05fd1",
+    "url": "/static/js/runtime-main.4ba1596e.js"
+  }
+]);
